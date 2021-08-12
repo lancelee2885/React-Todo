ab91a8b59fee4e9fa7cded964fc2023229cf451a
lancelee2885 <lancelee2885@gmail.com>
2021-08-12 15:15:28 -0700

    first commit

---
+++ plan.md
@@ -0,0 +1,27 @@
+Components:
+    - Todo App
+        - Combinantion of components
+
+    - ToDoForm
+        - should be reusable for add or edit
+
+    - Todos
+        - State: 
+            - # of todos
+        - Display all of todos
+
+    - Top Todo
+        - choose a todo that has highest priority and display it
+
+    - individual task
+        - Props:
+            - Title, Description, Priority
+
+    - footer
+        - render a footer
+
+    - App
+        - render Todo app
+
+    - Index
+        - Render app
---
+++ src/TodoApp.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+import TopTodo from "./TopTodo";
+import EditableTodoList from "./EditableTodoList";
+import TodoForm from "./TodoForm";
+
+/** App for managing a todo list.
+ *
+ * Props:
+ * - initialTodos: possible array of [ todo, ... ]
+ *
+ * State:
+ * - todos: array of [ todo, ... ]
+ *
+ * App -> TodoApp -> { TodoForm, EditableTodoList }
+ */
+
+function TodoApp({ initialTodos }) {
+  const [todos, setTodos] = useState(initialTodos);
+  
+  /** add a new todo to list */
+  function create(newTodo) {
+    setTodos(t => [...t, newTodo]);
+  }
+
+  /** update a todo with updatedTodo */
+  function update(updatedTodo) {
+    let todoCopy = [...todos];
+
+    for (let todo of todoCopy){
+      if (todo.id === updatedTodo.id){
+        todo.title = updatedTodo.title;
+        todo.description = updatedTodo.description;
+        todo.priority = updatedTodo.priority;
+      }
+    }
+    setTodos(todoCopy);
+  }
+
+  /** delete a todo by id */
+  function remove(id) {
+    let todoCopy = [...todos];
+    todoCopy = todoCopy.filter(t => t.id !== id);
+    setTodos(todoCopy);
+  }
+
+  return (
+      <main className="TodoApp">
+        <div className="row">
+
+          <div className="col-md-6">
+            <EditableTodoList 
+              update={update}
+              remove={remove}
+            /> OR
+            <span className="text-muted">You have no todos.</span>
+          </div>
+
+          <div className="col-md-6">
+            (if no top todo, omit this whole section)
+            <section className="mb-4">
+              <h3>Top Todo</h3>
+              <TopTodo />
+            </section>
+
+            <section>
+              <h3 className="mb-3">Add Nü</h3>
+              <TodoForm handleSave={create}/>
+            </section>
+          </div>
+
+        </div>
+      </main>
+  );
+}
+
+export default TodoApp;
